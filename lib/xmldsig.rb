0806dba9066c1601bf27730fe0223e35f1c7bea8
skiedra <jmindaugas17@yahoo.co.uk>
2017-07-17 10:59:16 +0300

    Update xmldsig.rb
    added namespace for body tag

--- lib/xmldsig.rb
+++ lib/xmldsig.rb
@@ -7,7 +7,8 @@ module Xmldsig
   NAMESPACES = {
       "ds"  => "http://www.w3.org/2000/09/xmldsig#",
       "ec"  => "http://www.w3.org/2001/10/xml-exc-c14n#",
-      "wsu" => "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+      "wsu" => "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd",
+      "SOAP-SEC" => "http://schemas.xmlsoap.org/soap/security/2000-12"
   }
 
   class Error < StandardError
